3ce0b7834d3c855cc36f3847b230df712695d049
queen-x-vee <valentinabuoro4@gmail.com>
2022-05-24 18:51:58 +0100

    this is an update

--- js/app.js
+++ js/app.js
@@ -1,125 +1,105 @@
 
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
-
-/*
- * Define Global Variables
-*/
-
-
-let section, menu, sectionArray, sectionLength;
-section = document.querySelectorAll('section')
-sectionArray = Array.from(section)
+let sections, menu, sectionArray, sectionLength;
+sections = document.querySelectorAll('section')
+sectionArray = Array.from(sections)
 sectionLength = sectionArray.length
 console.log(sectionLength)
 menu = document.querySelector('#navbar__list')
 
-
-
-
 // build the nav
-const Navigation = ()=>{
+const Navigation = () => {
     let list = ''
 
-    sectionArray.forEach((section)=>{
+    sectionArray.forEach((section) => {
         const sectionName = section.getAttribute('data-nav')
-        let sectionId = section.getAttribute('id')
+        var sectionId = section.getAttribute('id')
         //console.log( sectionName, sectionId)
-
         let list = document.createElement('li')
-        list.innerHTML = `<a class = 'menu__link'href = "#${sectionId}">${sectionName}</a>`
+        list.innerHTML = `<a class = 'menu__link ${sectionId}'href = "#${sectionId}">${sectionName}</a>`
         //list +=`<li><a href = "#${sectionId}">${sectionName}</a></li>`
         //menu.innerHTML = list
 
         menu.appendChild(list)
-
     })
 
-   
+
 
 }
 
 Navigation()
 
 
-
-
-
 //to get the position of each section
-const sectionPosition = (section)=>{
+const sectionPosition = (section) => {
 
-     return  Math.floor(section.getBoundingClientRect().top)
-    
+    return Math.floor(section.getBoundingClientRect().top)
+
 }
-
 //to remove the active class
-const isActive = (section)=>{
+const isActive = (section) => {
     section.classList.remove('your-active-class')
-    
+    //list.classList.remove('active')
 }
-
-
 //to add active class
-const addActive = (condition, section)=>{
-    if(condition){
+const addActive = (condition, section) => {
+    if (condition) {
         section.classList.add('your-active-class')
-        
+
     }
 }
 
+
+const lists = document.querySelectorAll('li')
 // Add class 'active' to section when the position is close to top of viewport
-
-const activation = ()=>{
-    section.forEach(section=>{
-        const elementPosition = sectionPosition (section)
-
-        inviewport = ()=> elementPosition <150 && elementPosition >= -150
-
+const activation = () => {
+    let current = ''
+    sections.forEach(section => {
+        //height
+        const elementPosition = sectionPosition(section)
+        inviewport = () => elementPosition < 150 && elementPosition >= -150
         isActive(section)
         addActive(inviewport(), section)
+        current = section.getAttribute('id')
     })
-
-
-
     
+    const listArray = Array.from(lists)
+    listArray.forEach(list => { 
+        list.classList.remove('active')
+        list.addEventListener('click', function myNavFunction(){
+            return list.classList.add('active') 
+        })
+        /*list.onclick = function myNavFunction(){
+             return list.classList.add('active') 
+         }*/
+    }) 
+
 }
 
 window.addEventListener('scroll', activation)
 
 
 // add classlist to naviagtion as per review
-document.querySelector("nav").onclick = function() {myNavFunction()};
+/*document.querySelector("li").onclick = //function() {myNavFunction()};
 function myNavFunction(){
-    var element = document.querySelector("nav");
-    element.classList.add("mystyle");
+    var element = document.querySelector("li");
+    element.classList.add("active");
+}*/
+
+const listActive = (list) => {
+    list.classList.remove('active')
+
 }
+//to add active class
 
-
-const scrolling = ()=>{
-    const links = document.querySelectorAll('a')
-    links.forEach(link=>{
-        link.addEventListener('click',(event)=>{
-            event.preventDefault()
-            for(let section of sectionArray)
-            //const element = document.getElementById("content");
-            section.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
-        })
+//const lists = document.querySelectorAll('li')
+/*const listArray = Array.from(lists)
+listArray.forEach(list => { 
+    list.addEventListener('click', function myNavFunction(){
+        return list.classList.add('active') 
     })
-}
+    /*list.onclick = function myNavFunction(){
+         return list.classList.add('active') 
+     }
+})*/
+
