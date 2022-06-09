66a337c5eb4f5786c9139e2d638b6d75e8b62049
queen-x-vee <valentinabuoro4@gmail.com>
2022-06-09 12:13:33 +0100

    9-6-2022 (1)

--- js/app.js
+++ js/app.js
@@ -13,14 +13,8 @@ const Navigation = () => {
     sectionArray.forEach((section) => {
         const sectionName = section.getAttribute('data-nav')
         const sectionId = section.getAttribute('id')
-        //console.log( sectionName, sectionId)
-        //list = document.createElement('li')
-        //list.innerHTML =  `  <a class ='menu__link ${sectionId}' href ="#${sectionId}">${sectionName}</a>`
         list +=`<li class = ${sectionId}> <a  class ='menu__link ${sectionId}' href = "#${sectionId}">${sectionName}</a></li>`
         menu.innerHTML = list
-
-        //menu.appendChild(list)
-        //console.log(sectionId)
     })
 
 
@@ -28,55 +22,36 @@ const Navigation = () => {
 }
 
 Navigation()
-
+const navigation = document.getElementById('navbar__list')
 const sectionPosition = (section) => {
     return Math.floor(section.getBoundingClientRect().top)
 }
 //to remove the active class
 const isActive = (section) => {
     section.classList.remove('your-active-class')
+    const sectionId = section.getAttribute('id').slice(7,8)-1
+    navigation.childNodes[sectionId].classList.remove('active')
 
 }
 //to add active class
 const addActive = (condition, section) => {
     if (condition) {
         section.classList.add('your-active-class')
-        console.log(section.getAttribute('id'))
+        const sectionId = section.getAttribute('id').slice(7,8)-1
+        navigation.childNodes[sectionId].classList.add('active')
     }
 }
 
 const lists = document.querySelectorAll('nav ul li')
 
+
 // Add class 'active' to section when the position is close to top of viewport
 const activate = () => {
-    //let current = ''
-    //let sectionName = ''
     sections.forEach(section => {
         const elementPosition = sectionPosition(section)
-        //console.log(elementPosition)
         inviewport = () =>  elementPosition < 150 && elementPosition >= -150
         isActive(section)
         addActive(inviewport(), section)
-        current = section.getAttribute('id')
-        //return current
     })
-    //console.log(current)
-    let curren = ''
-    sections.forEach(section=>{
-        const sectionTop = section.offsetTop
-        //console.log(sectionTop)
-        const sectionHeight = section.clientHeight;
-        if(scrollY >= sectionTop){
-            curren = section.getAttribute('id')
-        }
-    })
-    console.log(curren)
-    lists.forEach(list=>{
-        list.classList.remove('active')
-        if(list.classList.contains(curren)){
-            list.classList.add('active')
-        }
-    })
-
 }
 window.addEventListener('scroll', activate)
